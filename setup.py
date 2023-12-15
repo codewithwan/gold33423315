1daafd2a68d52e9bd40ef4b6ea967efc4732dcb8
codewithwan <hhhh25737@gmail.com>
2023-12-15 08:42:07 +0700

    second

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '1.0.0'
+VERSION = '0.0.2'
 DESCRIPTION = 'A Python library leveraging the OMDb API to fetch movie details based on user-provided titles. It also offers plot translation from English to Indonesian using googletrans.'
 
 this_directory = Path(__file__).parent
@@ -9,18 +9,18 @@ LONG_DESCRIPTION = (this_directory / 'README.md').read_text()
 
 # Setting up
 setup(
-    name="schmovie",
+    name="gold33423315",
     version=VERSION,
     author="codewithwan",
     author_email="<deezerxstore@gmail.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url='https://github.com/codewithwan/schmovie',
+    url='https://github.com/codewithwan/gold33423315',
     packages=find_packages(),
     license='MIT',
     install_requires=[],
-    keywords=['SCH', 'OMDb'],
+    keywords=['UAs', 'COY'],
     classifiers=[
         'Development Status :: 1 - Planning',
     ],
